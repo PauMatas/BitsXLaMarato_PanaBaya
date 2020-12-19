1dfada3f18137a226fc1e34bdc372f2350656bf3
pTarNav <pautarragonavarra@gmail.com>
2020-12-19 11:33:30 +0100

    parser structure

--- data_parser.py
+++ data_parser.py
@@ -1,3 +1,27 @@
 import pandas as pd
 
 inpdf = pd.read_csv("COPEDICATClinicSympt_DATA_2020-12-17_1642.csv")
+dd = [[]]
+
+"""
+name [type] (col in df)
+"""
+def parser_by_row(patient_id, l):
+    patient_data = []
+
+    # patient_id [int] (0)
+    patient_data.append(patient_id)
+
+    # sanitary_id [string] (1)
+    patient_data.append(l[1])
+
+    dd.append(patient_data)
+
+
+def parse():
+    for i in range(len(inpdf)) : 
+        parser_by_row(i, inpdf.iloc[i])
+
+    print(dd)
+
+parse()
