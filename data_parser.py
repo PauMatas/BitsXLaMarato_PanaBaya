c809ea82a10543fd3f51817e5b46969187c7b90c
Pau Matas <pmatasalbiol@gmail.com>
2020-12-19 13:35:44 +0100

    parser update

--- data_parser.py
+++ data_parser.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 
 inpdf = pd.read_csv("COPEDICATClinicSympt_DATA_2020-12-17_1642.csv")
-dd = [[]]
+dd = []
 useful_columns = [3, 19, 25, 26, 31, 32, 34, 36, 39, 43, 45, 47, 49, 53, 55,
                   57, 59, 61, 65, 70, 75, 77, 79]
 binary_columns = [19, 25, 26, 31, 32, 34, 65, 70]
@@ -11,16 +12,16 @@ binary_w_uknw_cols = [36, 39, 43, 45, 47, 49, 53, 55, 57, 59, 61, 75, 77, 79]
 name [type] (col in df)
 """
 
-def make_up(x):
+def make_up(x, l):
     val = l[x]
 
-    if val.isnull():
+    if isinstance(val, str) or np.isnan(val):
         if val in binary_columns:
-            val = 0
+            return 0
         elif val in binary_w_uknw_cols:
-            val = 3
+            return 3
         else:
-            val = 0
+            return 0
 
     return val
 
@@ -30,17 +31,22 @@ def parser_by_row(patient_id, l):
     # patient_id [int] (0)
     patient_data.append(patient_id)
 
-    patient_data += [make_up(x) for x in useful_columns]
+    patient_data += [make_up(x-1, l) for x in useful_columns]
 
     # sanitary_id [string] (1)
     patient_data.append(l[1])
 
-    dd.append(patient_data)
+    return patient_data
 
 
 def parse():
+
     dd = [parser_by_row(i, inpdf.iloc[i]) for i in range(len(inpdf))]
 
-    print(dd)
+    return dd
 
-parse()
+dd = parse()
+
+headers = ['id'] + [inpdf.columns[i] for i in range(len(inpdf.columns)) if i+1 in useful_columns] + ['_']
+
+pd.DataFrame(dd).to_csv('clean_data.csv', header = headers)
