ae87a187b02bff7f1af97ad5b66d841692221741
Tomás Gadea <01tomas.gadea@gmail.com>
2020-12-20 11:18:26 +0100

    checkpoint de tomas

--- model_training.py
+++ model_training.py
@@ -77,3 +77,8 @@ print('MAE:', val_mae)
 
 scores = cross_val_score(random_forest, X_val, y_val, cv=5)
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
+
+def get_model():
+    return random_forest
