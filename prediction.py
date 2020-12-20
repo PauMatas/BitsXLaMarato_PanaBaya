0550074689b14b56266069f04a4c0507c22cd0c9
pTarNav <pautarragonavarra@gmail.com>
2020-12-20 11:24:47 +0100

    asd

--- prediction.py
+++ prediction.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
-import data_cleaner
-from joblib import dump, load
+import data_cleaner as dc
+import model_training as mt
 
 def data_predict(file_name):
-	X_val = clean_data(file_name, False, False)
-	random_forest = load('model.joblib')
-	
+	X_val = dc.clean_data(file_name, False, False)
+	random_forest = mt.get_model()
+	feat = mt.get_features()
+	X_val = X_val.loc[:,feat]
+
 	y_pred = random_forest.predict(X_val)
+	print(y_pred)
+
+data_predict("COPEDICATClinicSympt_DATA_2020-12-17_1642.csv")
